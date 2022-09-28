279b5b79dbc91673e09f8053b140e1dd98a0147c
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-28 14:15:20 +0000

    metro updates

--- metro.config.js
+++ metro.config.js
@@ -28,5 +28,18 @@ module.exports = MetroConfig.create(evaConfig, {
         inlineRequires: false,
       },
     }),
+    getPolyfills: () => [
+      require.resolve('react-native/Libraries/polyfills/console.js'),
+      require.resolve('react-native/Libraries/polyfills/error-guard.js'),
+      require.resolve('react-native/Libraries/polyfills/Number.es6.js'),
+      require.resolve(
+        'react-native/Libraries/polyfills/String.prototype.es6.js',
+      ),
+      require.resolve(
+        'react-native/Libraries/polyfills/Array.prototype.es6.js',
+      ),
+      require.resolve('react-native/Libraries/polyfills/Array.es6.js'),
+      require.resolve('react-native/Libraries/polyfills/Object.es7.js'),
+    ],
   },
 });
